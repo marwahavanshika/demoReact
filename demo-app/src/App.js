ff5f188c6e841085a12bab38cb2e6e7e228f6569
Vanshika Marwaha <vmarwaha02@gmail.com>
2023-02-14 02:12:27 +0530

    edited the render function in app.js and in table.js i passed the removeCharacter function as a property to the table

--- demo-app/src/App.js
+++ demo-app/src/App.js
@@ -12,33 +12,67 @@ import Table from './table'
 // }
 
 
+// class App extends Component {
+//   render() {
+//     const characters = [
+//       {
+//         name: 'name1',
+//         job: 'Job1',
+//       },
+//       {
+//         name: 'name2',
+//         job: 'job2',
+//       },
+//       {
+//         name: 'name3',
+//         job: 'job2',
+//       },
+//       // {
+//       //   name: 'name4',
+//       //   job: 'Bartender',
+//       // },
+//     ]
+
+
+//     return (
+//       <div className="container">
+//         <Table characterData={characters} />
+//       </div>
+//     )
+//   }
+// }
+
+//states
+
 class App extends Component {
-  render() {
-    const characters = [
+  state = {
+    characters: [
+
       {
         name: 'name1',
         job: 'Job1',
-      },
-      {
+       },
+       {
         name: 'name2',
-        job: 'job2',
+         job: 'job2',
+       },
+       {
+         name: 'name3',
+          job: 'job3',
       },
-      {
-        name: 'name3',
-        job: 'job2',
-      },
-      // {
-      //   name: 'name4',
-      //   job: 'Bartender',
-      // },
-    ]
 
-    return (
-      <div className="container">
-        <Table characterData={characters} />
-      </div>
-    )
+    ]
   }
+}
+
+removeCharacter = (index) => {
+  const { characters } = this.state
+
+  this.setState({
+    characters: characters.filter((character, i) => {
+      return i !== index
+    }),
+  })
 }
 
 export default App
